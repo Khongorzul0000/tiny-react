91321e7aaacff34c2b8d02f6843d85b4fecff817
22PHOP2D-Khongorzul <Khongorzul@Khulangoos-iMac-2.local>
2023-02-18 19:19:04 +0800

    2 sariin 18

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,15 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export const Home = () => {
   const [url, setUrl] = useState("");
   const [baba, setBaba] = useState("");
   const [find, setFind] = useState([]);
-  const [what, setWhat] = useState(false)
+  const [id, setId] = useState([]);
+  const [get, setGet] = useState([]);
+  const [what, setWhat] = useState(false);
 
   const shortUrl = async () => {
     const newUrl = { url: url, baba: baba };
@@ -25,55 +27,58 @@ export const Home = () => {
       .then((res) => {
         // console.log(res.data.shortUrl)
         setBaba(res.data.shortUrl);
+        setId(res.data._id);
         console.log(baba);
+        console.log(id);
       })
       .catch((error) => {
         console.log(error);
       });
-
-    // axios
-    // .get(`http://localhost:3001/${baba}`)
-    // .then((res)=>{
-    //   console.log(res)
-    // }).catch(err => {
-    //   console.log(err)
-    // })
   };
+  const dltBnt = async () => {
+    axios.delete(`http://localhost:3001/${id}`).then((res) => {
+      console.log("delete bolchihloo");
+    });
+  };
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001")
+      .then((res) => {
+        setGet(res.data);
+      })
+      .catch(console.log);
+  }, []);
 
   return (
-    <div className={styles.back}>
-      <div>
-        <div className={styles.flex}>
-          <input
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            className={styles.npt}
-          ></input>
-          <button onClick={shortUrl} className={styles.btn}>
-            short
-          </button>
-        </div>
-
-        <div className={styles.map}>
-          {find.map((give) => {
-            console.log(give);
-            return (
-              <div className={styles.gap}>
-                <div className={styles.full}>
-                  <p className={styles.url}>full url</p>
-
-                  <a>{give.url}</a>
-                </div>
-                <div className={styles.ful}>
-                  <p className={styles.url}>short url</p>
-                  <a>http://localhost:3001/{give.baba} </a>
-                  <CopyToClipboard text={baba} onCopy={() => setWhat(true)}>
-                  <button>bdmnvb</button>
-                  </CopyToClipboard>
-                </div>
-              </div>
-            );
-          })}
+    <div className={styles.background}>
+      <div className={styles.back}>
+        <div>
+          <div className={styles.flex}>
+            <input
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              className={styles.npt}
+            ></input>
+            <button onClick={shortUrl} className={styles.btn}>
+              short
+            </button>
+          </div>
+          <div className={styles.map}>
+            <div className={styles.jjj}>
+              {find.map((give) => {
+                console.log(give);
+                return (
+                
+                  <div className={styles.block}>
+                    <a className={styles.local}>{give.url}</a>
+                   <hr></hr>
+                    <a className={styles.local}>http://localhost:3001/{give.baba}</a>
+                  </div>
+              
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
